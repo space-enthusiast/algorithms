00c06f52e63afa5609b9a040f8381154ccae4442
space-enthusiast <jinding0829@gmail.com>
2023-07-16 14:38:42 +0900

    [TEMPLATE] default cpp template

---
+++ template/template.cpp
@@ -0,0 +1,26 @@
+#define FOR(N, I) for(I=0;I<N;I++)
+#define PRT(A) count<<(A);
+#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]; } count<<endl; }
+#define PRT_ENTER cout<<endl;
+#define INP_INT_INT(N, M) cin>>n>>m;
+#define LAST_IDX(N) N-1
+#define FIRST_IDX 0
+#define AND &&
+#define OR ||
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void setup() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {
+    setup();
+
+    int i,j,n,m;
+
+    cout<<"hello world"<<endl;
+}
