5ad1963db00bce9800fa6af346058be752dc02b3
space-enthusiast <jinding0829@gmail.com>
2023-07-16 15:46:57 +0900

    [TEMPLATE] template update endl performance issue

--- template/template.cpp
+++ template/template.cpp
@@ -1,8 +1,7 @@
 #define FOR(N, I) for(I=0;I<N;I++)
 #define PRT(A) count<<(A);
-#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]; } count<<endl; }
-#define PRT_ENTER cout<<endl;
-#define INP_INT_INT(N, M) cin>>n>>m;
+#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]; } count<<"\n"; }
+#define PRT_ENTER cout<<"\n";
 #define LAST_IDX(N) N-1
 #define FIRST_IDX 0
 #define AND &&
