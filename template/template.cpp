d848de197686415944faae471c353f83af625919
space-enthusiast <jinding0829@gmail.com>
2023-07-26 23:34:36 +0900

    [TEMPLATE] debug, max, inf added

--- template/template.cpp
+++ template/template.cpp
@@ -3,12 +3,15 @@
 #define PRT(A) cout<<(A);
 #define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]; } cout<<"\n"; }
 #define PRT_ENTER cout<<"\n";
+#define PRT_DEBUG_LINE cout<<'\n'<<"================"<<'\n';
 #define LAST_IDX(N) N-1
 #define FIRST_IDX 0
 #define AND &&
 #define OR ||
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #define top_pop(pq) (pq).top();(pq).pop();
+#define MAX 500
+#define INF 2147483647
 
 #include <bits/stdc++.h>
 using namespace std;
