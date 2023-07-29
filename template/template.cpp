629ea71976252f89c815aaf21295a1340f4eeb36
space-enthusiast <jinding0829@gmail.com>
2023-07-29 15:39:58 +0900

    [TEMPLTAE] reformat template code and added more define statements

--- template/template.cpp
+++ template/template.cpp
@@ -1,25 +1,28 @@
 #define FOR_N(N, I) for(I=0;I<N;I++)
 #define FOR_N_M(N, M, I) for(I=N;I<=M;I++)
 #define PRT(A) cout<<(A);
-#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]; } cout<<"\n"; }
+#define PRT_ARY_1D(ARR, N, I) for(I=0;I<N;I++){ cout<<ARR[I]<<' '; } cout<<"\n";
+#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]<<' '; } cout<<"\n"; }
 #define PRT_ENTER cout<<"\n";
 #define PRT_DEBUG_LINE cout<<'\n'<<"================"<<'\n';
 #define LAST_IDX(N) N-1
 #define FIRST_IDX 0
 #define AND &&
 #define OR ||
+#define NOT !
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #define top_pop(pq) (pq).top();(pq).pop();
-#define MAX 500
+#define MAX 100000
 #define INF 2147483647
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
     FASTIO
 
-    int i,j,n,m;
+    int i, j, n, m;
 
-    cout<<"hello world"<<endl;
+    cout << "hello world" << endl;
 }
