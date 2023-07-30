f438bdbc1794ce9df736bba3545932adeeacf3fd
space-enthusiast <jinding0829@gmail.com>
2023-07-30 13:19:05 +0900

    [problem] [boj] [1269] set calculation practice good question

---
+++ map_set/boj1269.cpp
@@ -0,0 +1,49 @@
+#define FOR_N(N, I) for(I=0;I<N;I++)
+#define FOR_N_M(N, M, I) for(I=N;I<=M;I++)
+#define PRT_SET(SET) for (auto i: SET) { cout << i << ' '; } cout<<"\n";
+#define PRT_SET_NAME(SET_NAME, SET) cout<<setw(6)<<SET_NAME<<": "; for (auto SET_ITER: SET) { cout << SET_ITER << ' '; } cout<<"\n";
+#define PRT(A) cout<<(A)<<'\n';
+#define PRT_NAME(A_NAME, A) cout<<(A_NAME)<<": "<<(A)<<'\n';
+#define PRT_ARY_1D(ARR, N, I) for(I=0;I<N;I++){ cout<<ARR[I]<<' '; } cout<<"\n";
+#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]<<' '; } cout<<"\n"; }
+#define PRT_ENTER cout<<"\n";
+#define PRT_DEBUG_LINE cout<<'\n'<<"================"<<'\n';
+#define LAST_IDX(N) (N-1)
+#define FIRST_IDX 0
+#define AND &&
+#define OR ||
+#define NOT !
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define top_pop(pq) (pq).top();(pq).pop();
+#define MAX 100000
+#define INF 2147483647
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    FASTIO
+
+    int n, m, i, temp;
+    set<int> a, b;
+    set<int> amb, bma;
+
+    cin >> n >> m;
+
+    FOR_N(n, i) {
+        cin >> temp;
+        a.insert(temp);
+    }
+    FOR_N(m, i) {
+        cin >> temp;
+        b.insert(temp);
+    }
+
+    set_difference(a.begin(), a.end(), b.begin(), b.end(), inserter(amb, amb.begin()));
+    set_difference(b.begin(), b.end(), a.begin(), a.end(), inserter(bma, bma.begin()));
+    int ambs = amb.size();
+    int bmas = bma.size();
+
+    cout << ambs + bmas;
+}
