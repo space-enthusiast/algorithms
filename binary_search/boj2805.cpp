2dcf6a3deace88f2c7d260b7a3d4014faeca5cb9
space-enthusiast <jinding0829@gmail.com>
2023-08-02 00:19:03 +0900

    [problem] [boj] [2805] first Parametric Search problem

---
+++ binary_search/boj2805.cpp
@@ -0,0 +1,63 @@
+#define FOR_N(N, I) for(I=0;I<N;I++)
+#define FOR_N_M(N, M, I) for(I=N;I<=M;I++)
+#define PRT_SET(SET) for (auto SET_ITER: SET) { cout << SET_ITER << ' '; } cout<<"\n";
+#define PRT_SET_NAME(SET_NAME, SET) cout<<setw(6)<<SET_NAME<<": "; for (auto SET_ITER: SET) { cout << SET_ITER << ' '; } cout<<"\n";
+#define PRT(A) cout<<(A)<<'\n';
+#define PRT_NAME(A_NAME, A) cout<<(A_NAME)<<": "<<(A)<<'\n';
+#define PRT_ARY_1D(ARR, N, I) for(I=0;I<N;I++){ cout<<ARR[I]<<' '; } cout<<"\n";
+#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]<<' '; } cout<<"\n"; }
+#define PRT_ENTER cout<<"\n";
+#define PRT_DEBUG_LINE cout<<'\n'<<"================"<<'\n';
+#define LAST_IDX(N) (N-1)
+#define FIRST_IDX 0
+#define AND &&
+#define OR ||
+#define NOT !
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define top_pop(pq) (pq).top();(pq).pop();
+#define MAX 1000000
+#define INF numeric_limits<long>::max()
+#define NUM long long int
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+NUM left_tree(NUM *arr, NUM n, NUM cut_size) {
+    NUM i, sum = 0;
+    FOR_N(n, i) {
+        NUM res = arr[i] - cut_size;
+        if (res >= 0) {
+            sum += res;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    FASTIO
+
+    NUM n, m, i;
+    NUM arr[MAX];
+
+    cin >> n >> m;
+    FOR_N(n, i) {
+        cin >> arr[i];
+    }
+
+    NUM start = 1;
+    NUM end = INF / 2;
+    NUM ans = INF / 2;
+
+    while (start <= end) {
+        NUM mid = (start + end) / 2;
+        if (left_tree(arr, n, mid) >= m) {
+            start = mid + 1;
+        } else {
+            ans = min(ans, mid - 1);
+            end = mid - 1;
+        }
+    }
+
+    cout << ans;
+}
