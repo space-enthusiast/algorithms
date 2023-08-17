08c8a778a8483be274946a7843058e1c38598f20
space-enthusiast <jinding0829@gmail.com>
2023-08-18 08:20:09 +0900

    [problem] [boj] [24416] easy dp

---
+++ dp/boj24416.cpp
@@ -0,0 +1,47 @@
+#define FOR_N(N, I) for(I=0;I<N;I++)
+#define FOR_N_M(N, M, I) for(I=N;I<=M;I++)
+#define PRT_SET(SET) for (auto SET_ITER: SET) { cout << SET_ITER << ' '; } cout<<"\n";
+#define PRT_SET_NAME(SET_NAME, SET) cout<<setw(6)<<SET_NAME<<": "; for (auto SET_ITER: SET) { cout << SET_ITER << ' '; } cout<<"\n";
+#define PRT(A) cout<<(A)<<'\n';
+#define PRT_NAME(A_NAME, A) cout<<(A_NAME)<<": "<<(A)<<'\n';
+#define PRT_ARY_1D(ARR, N, I) for(I=0;I<N;I++){ cout<<ARR[I]<<' '; } cout<<"\n";
+#define PRT_ARY_2D(ARR, N, M, I, J) for(I=0;I<N;I++){ for(J=0;J<M;J++) { cout<<ARR[I][J]<<' '; } cout<<"\n"; }
+#define PRT_ENTER cout<<"\n";
+#define PRT_DEBUG_LINE cout<<'\n'<<"================"<<'\n';
+#define LAST_IDX(N) (N-1)
+#define FIRST_IDX 0
+#define AND &&
+#define OR ||
+#define NOT !
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define top_pop(pq) (pq).top();(pq).pop();
+#define MAX 40
+#define INF numeric_limits<long>::max()
+#define NUM long long int
+#define INI_ARR_NUM(ARR, N) NUM ARR[N]; memset(arr, 0, sizeof(ARR));
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+NUM n, m;
+NUM i, j, k;
+
+int main() {
+    FASTIO
+
+    cin >> n;
+    INI_ARR_NUM(arr, n)
+
+    arr[1] = 1;
+    arr[2] = 1;
+    arr[3] = 2;
+    arr[4] = 3;
+    FOR_N_M(5, n, i) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    cout << arr[n];
+    cout << ' ';
+    cout << n - 2;
+}
